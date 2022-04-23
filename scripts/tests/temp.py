d7cd8e4b78cb8f2f791a98b1e9196663167bb85e
jcschindler01 <jcschindler01@gmail.com>
2022-04-23 10:51:16 +0200

    Update template to current project.

--- scripts/tests/temp.py
+++
@@ -1,6 +0,0 @@
-
-import skeleton
-
-print("skeleton version = %s"%(skeleton.__version__))
-
-
--- src/multipartite/__init__.py
+++ src/multipartite/__init__.py
@@ -5,5 +5,4 @@ Hello! I am the comment heading the top level __init__.py in skeleton.
 from importlib.metadata import version
 __version__ = version("skeleton-JCSCHINDLER01")
 
-from skeleton import sub1
-from skeleton import sub2
+
