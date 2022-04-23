2ca452a4de10d1b23dd0f812f7b84e8ca13c082f
jcschindler01 <jcschindler01@gmail.com>
2022-04-23 10:58:02 +0200

    Folder to move tests to from main module.

--- src/multipartite/__init__.py
+++ src/multipartite/__init__.py
@@ -3,6 +3,6 @@ Hello! I am the comment heading the top level __init__.py in skeleton.
 """
 
 from importlib.metadata import version
-__version__ = version("skeleton-JCSCHINDLER01")
+__version__ = version("multipartite")
 
 
