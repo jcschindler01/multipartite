fe7eeb4a9433223e7952bfb3a1cc34e2d58f491f
jcschindler01 <jcschindler01@gmail.com>
2022-04-24 15:23:31 +0200

    Add commonly used sigma, psi, rho, as constant values.

--- src/multipartite/constants.py
+++ src/multipartite/constants.py
@@ -1,5 +1,5 @@
 
-
+import numpy as np
 
 ## sigma matrices
 I = np.array([[1,0],[0,1]], dtype=complex)
@@ -8,5 +8,29 @@ Y = np.array([[0,-1j],[1j,0]], dtype=complex)
 Z = np.array([[1,0],[0,-1]], dtype=complex)
 sigma = np.stack([I,X,Y,Z])
 
+## vectors
+psid = dict()
+psid['+z'] = np.array([1, 0 ], dtype=complex)
+psid['-z'] = np.array([0, 1 ], dtype=complex)
+psid['+x'] = np.array([1, 1 ], dtype=complex)
+psid['-x'] = np.array([1,-1 ], dtype=complex)
+psid['+y'] = np.array([1, 1j], dtype=complex)
+psid['-y'] = np.array([1,-1j], dtype=complex)
+psid['0'] = psid['+z']
+psid['1'] = psid['-z']
+psid['+'] = psid['+x']
+psid['-'] = psid['-x']
 
+## normalize vectors
+for key in psid.keys():
+  psid[key] = psid[key]/np.sqrt( np.conjugate(np.transpose(psid[key]))@psid[key] )
+
+## density matrices
+rhod = dict()
+for key in psid.keys():
+  rhod[key] = np.outer(psid[key], np.conjugate(psid[key]))
+
+for key in rhod.keys():
+  print(key)
+  print(np.round(rhod[key],3))
 
