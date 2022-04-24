7c3bad05abe5697f4104d020a96b0702ac3ccae3
jcschindler01 <jcschindler01@gmail.com>
2022-04-24 15:24:17 +0200

    Make faster by losing excess imports for now.

--- src/multipartite/__init__.py
+++ src/multipartite/__init__.py
@@ -5,11 +5,11 @@ Hello! I am the comment heading the top level __init__.py in skeleton.
 from importlib.metadata import version
 __version__ = version("multipartite")
 
-import numpy as np
-import matplotlib.pyplot as plt
-plt.style.use("classic")
 
-from multipartite import mptools
-from multipartite import blochvec
-from multipartite.mptools import *
-from multipartite.blochvec import *
+# from matplotlib.pyplot import style
+# plt.style.use("classic")
+
+#from multipartite import mptools
+#from multipartite import blochvec
+#from multipartite.mptools import *
+#from multipartite.blochvec import *
