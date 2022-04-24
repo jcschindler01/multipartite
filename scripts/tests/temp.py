27cdd86acb6edbf041fd6973409083a3e81ead10
jcschindler01 <jcschindler01@gmail.com>
2022-04-24 20:07:02 +0200

    Building script using new LO modules.

--- scripts/tests/temp.py
+++ scripts/tests/temp.py
@@ -7,4 +7,15 @@ from multipartite.matrix import *
 # import matplotlib.pyplot as plt
 # plt.style.use("classic")
 
+rho = np.eye(4)/4.
+
+A = [projd['0'],projd['1']]
+B = [projd['0'],projd['1']]
+
+m1 = slo(rho,A,B)
+
+rde = m1['RDE_out'].copy()
+
+
+
 
