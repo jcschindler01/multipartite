583fc1a7412315c0654b94b05084ad870f6b0f31
jcschindler01 <jcschindler01@gmail.com>
2022-05-07 18:45:11 +0200

    Create module to build LO tool. First need to add 2x2reduce and Bloch4 tools to matrix class.

---
+++ src/multipartite/tools/LO.py
@@ -0,0 +1,15 @@
+"""Tools for calculating entropy of LO povm measurement on a state rho."""
+
+class LO:
+  """Class representing LO povm measurement on a state rho."""
+  def __init__(self):
+    pass
+
+
+
+
+
+
+
+
+
