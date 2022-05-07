38598875e5d276d27a761631bc246ef5743b5dd9
jcschindler01 <jcschindler01@gmail.com>
2022-05-07 18:46:43 +0200

    Same as last one, needed to hit save...

--- src/multipartite/tools/LO.py
+++ src/multipartite/tools/LO.py
@@ -1,8 +1,11 @@
 """Tools for calculating entropy of LO povm measurement on a state rho."""
 
+import numpy as np
+from multipartite.tools import dd
+
 class LO:
-  """Class representing LO povm measurement on a state rho."""
-  def __init__(self):
+  """Class representing LO POVM measurement on a state rho."""
+  def __init__(self, rho=dd.matrix(), povm=dd.POVM(), dAB=(0,0)):
     pass
 
 
